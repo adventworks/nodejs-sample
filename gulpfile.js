5073744f3ecc49b8bf91fa36977467ebea718310
Mateo Escobedo <mat.esc@outlook.com>
2017-06-30 00:11:36 -0400

    Mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,8 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var minimist = require('minimist');
 var fs = require('fs');
 
-gulp.task('test', function () {
-    gulp.src('test/**/*.js')
-        .pipe(mocha({
-            reporter: 'nyan',
-            clearRequireCache: true,
-            ignoreLeaks: true
-        }));
+gulp.task('default', function () {
+    gulp.src('tests/**/*.js')
+        .pipe(mocha({reporter: 'mocha-junit-reporter'}));
 });
-
-gulp.task('default', 
