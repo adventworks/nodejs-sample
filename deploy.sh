90fd292374da0f6b76fe1498df88062d1dd80815
Mateo Escobedo <mat.esc@outlook.com>
2017-11-09 14:08:28 -0500

    Deloy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,22 @@
 # Script to deploy the app
 # This file is needed only when you want to deploy the app to a Linux VM
-sudo apt-get -y install npm
-sudo apt-get -y install nodejs
-npm init
-npm install express -y
+sudo apt-get update
+sudo apt-get install git
+
+# install node
+wget https://nodejs.org/dist/v4.2.3/node-v4.2.3-linux-x64.tar.gz
+mkdir node
+tar xvf node-v*.tar.?z --strip-components=1 -C ./node
+rm -rf node-v*
+mkdir node/etc
+echo 'prefix=/usr/local' > node/etc/npmrc
+sudo mv node /opt/
+sudo chown -R root: /opt/node
+sudo ln -s /opt/node/bin/node /usr/local/bin/node
+sudo ln -s /opt/node/bin/npm /usr/local/bin/npm
+
+# install express
+sudo npm install express -y
+
+# run server
 nodejs server.js
