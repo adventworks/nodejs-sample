c610c022bf1f669a855dc9674db97c06fc2caf12
Mateo Escobedo <mat.esc@outlook.com>
2017-11-14 09:30:06 -0500

    Make the deploy.sh idempotent

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,34 @@
 # Script to deploy the app
 # This file is needed only when you want to deploy the app to a Linux VM
-sudo apt-get update
-sudo apt-get install git
+sudo add-apt-repository ppa:nginx/stable -y
+sudo apt-get update -y
+sudo apt-get install git -y
 
 # install node
-wget https://nodejs.org/dist/v4.2.3/node-v4.2.3-linux-x64.tar.gz
+wget https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.gz
+rm -rf node
 mkdir node
 tar xvf node-v*.tar.?z --strip-components=1 -C ./node
 rm -rf node-v*
 mkdir node/etc
 echo 'prefix=/usr/local' > node/etc/npmrc
-sudo mv node /opt/
+sudo rm -rf /opt/node
+sudo mv -f node /opt/
 sudo chown -R root: /opt/node
-sudo ln -s /opt/node/bin/node /usr/local/bin/node
-sudo ln -s /opt/node/bin/npm /usr/local/bin/npm
+sudo ln -fs /opt/node/bin/node /usr/local/bin/node
+sudo ln -fs /opt/node/bin/npm /usr/local/bin/npm
 
 # install express
 sudo npm install express -y
 
-# run server
-nodejs server.js&
+# install nginx
+sudo apt-get install nginx -y
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo cp -f ./nginx-config /etc/nginx/sites-available/example
+sudo ln -fs /etc/nginx/sites-available/example /etc/nginx/sites-enabled
+sudo service nginx reload
+
+# install pm2 and start the node server as a daemon
+sudo npm install pm2 -g -y
+pm2 delete example -s
+pm2 start server.js -n example
