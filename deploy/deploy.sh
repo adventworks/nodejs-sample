a9f4168c94b69b1fc4744362ba91ca49c7a9deda
Mateo Escobedo <mat.esc@outlook.com>
2017-11-16 22:52:17 -0500

    Ubunti 16.04

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,34 +1,26 @@
 # Script to deploy the app
 # This file is needed only when you want to deploy the app to a Linux VM
-sudo add-apt-repository ppa:nginx/stable -y
-sudo apt-get update -y
-sudo apt-get install git -y
 
 # install node
-wget https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.gz
-rm -rf node
-mkdir node
-tar xvf node-v*.tar.?z --strip-components=1 -C ./node
-rm -rf node-v*
-mkdir node/etc
-echo 'prefix=/usr/local' > node/etc/npmrc
-sudo rm -rf /opt/node
-sudo mv -f node /opt/
-sudo chown -R root: /opt/node
-sudo ln -fs /opt/node/bin/node /usr/local/bin/node
-sudo ln -fs /opt/node/bin/npm /usr/local/bin/npm
+pushd .
+cd ~
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs -y
+sudo apt-get install build-essential -y
 
 # install express
 sudo npm install express -y -g
+popd
 
-# install nginx
-sudo apt-get install nginx -y
+# configure nginx
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp -f ./nginx-config /etc/nginx/sites-available/example
 sudo ln -fs /etc/nginx/sites-available/example /etc/nginx/sites-enabled
 sudo service nginx reload
 
 # install pm2 and start the node server as a daemon
+cd ..
 sudo npm install pm2 -g -y
 pm2 delete example -s
 pm2 start server.js -n example
